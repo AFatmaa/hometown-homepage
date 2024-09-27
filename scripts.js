e17bc641392f510023d182277d71ee4255d64934
AFatmaa <fatma.arslantas@outlook.com>
2024-09-27 16:52:38 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,36 @@
+function changeTitle() {
+    let title = document.getElementById('main-title');
+    title.innerHTML = 'Welcome to Istanbul!';
+}
+
+
+const images = [
+    ["hagia-sophia.jpg", "hagia-sophia2.jpg", "hagia-sophia3.jpg"],
+    ["maidens-tower.jpg", "maidens-tower2.jpg", "maidens-tower3.jpg"],
+    ["galata.jpg", "galata2.jpg", "galata3.jpg"]
+];
+
+let currentIndices = [0, 0, 0];
+
+function nextImage(carouselIndex) {
+    currentIndices[carouselIndex]++;
+    if (currentIndices[carouselIndex] >= images[carouselIndex].length) {
+        currentIndices[carouselIndex] = 0;
+    }
+    updateCarouselImage(carouselIndex);
+}
+
+
+function prevImage(carouselIndex) {
+    currentIndices[carouselIndex]--;
+    if (currentIndices[carouselIndex] < 0) {
+        currentIndices[carouselIndex] = images[carouselIndex].length - 1;
+    }
+    updateCarouselImage(carouselIndex);
+}
+
+
+function updateCarouselImage(carouselIndex) {
+    const imgElement = document.getElementById(`carousel-img-${carouselIndex}`);
+    imgElement.src = images[carouselIndex][currentIndices[carouselIndex]];
+}
